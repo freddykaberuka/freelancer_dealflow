78d866133e88e7e796b9806da11e8e09c78bbcfe
freddy <freddykaberuka2009@gmail.com>
2023-06-21 12:32:43 +0200

    add filtering by name

--- src/components/FreelancerList.tsx
+++ src/components/FreelancerList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 interface Freelancer {
   id: number;
@@ -15,18 +15,37 @@ interface FreelancerListProps {
 }
 
 const FreelancerList: React.FC<FreelancerListProps> = ({ freelancers }) => {
+  const [filter, setFilter] = useState('');
+
+  const filteredFreelancers = freelancers.filter((freelancer) =>
+    freelancer.names.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilter(event.target.value);
+  };
+
   return (
     <div>
       <h2>Freelancer List</h2>
+      <div>
+        <label htmlFor="filter">Filter by Name:</label>
+        <input
+          type="text"
+          id="filter"
+          value={filter}
+          onChange={handleFilterChange}
+        />
+      </div>
       <ul>
-        {freelancers.map((freelancer) => (
-          <li key={freelancer.id}>
-            <p>Name: {freelancer.names}</p>
-            <p>About: {freelancer.about}</p>
-            <p>Location: {freelancer.location}</p>
-            <p>Category: {freelancer.category}</p>
-            <p>Technologies: {freelancer.technologies.join(', ')}</p>
-            <img src={freelancer.image} alt={freelancer.names} />
+          {filteredFreelancers.map((freelancer) => (
+            <li key={freelancer.id}>
+              <p>Name: {freelancer.names}</p>
+              <p>About: {freelancer.about}</p>
+              <p>Location: {freelancer.location}</p>
+              <p>Category: {freelancer.category}</p>
+              <p>Technologies: {freelancer.technologies.join(', ')}</p>
+              <img src={freelancer.image} alt={freelancer.names} />
           </li>
         ))}
       </ul>
